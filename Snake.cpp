a917beeb0ffc4c7f948e714d404ef6c993affe01
supli6669 <25520320@gm.uit.edu.vn>
2026-04-04 23:09:10 +0700

    them khung

--- Snake.cpp
+++ Snake.cpp
@@ -4,7 +4,10 @@
 #include <conio.h>
 #include <vector>
 using namespace std;
+
 void gotoxy( int column, int line );
+void VeKhung(int rong, int cao); // Khai báo hàm vẽ khung
+
 struct Point{
     int x,y;
 };
@@ -14,27 +17,27 @@ enum State { BOOTING, MAIN_MENU, IN_GAME, CRITICAL_FAILURE };
 
 class CONRAN{
 public:
-   
     vector<Point> snake; 
     void InitSnake() {
-    snake.clear();
-    for (int i = 0; i < 5; i++) snake.push_back({ 20 - i, 12 });
-}
+        snake.clear();
+        for (int i = 0; i < 5; i++) snake.push_back({ 20 - i, 12 });
+    }
+    
     void Ve() {
         for (size_t i = 0; i < snake.size(); i++) {
             gotoxy(snake[i].x, snake[i].y);
             if (i == 0) cout << "O"; 
-            else cout << "x";       
+            else cout << "x";        
         }
     }
 
     void DiChuyen(Dir direction) {
-       
+
         for (size_t i = snake.size() - 1; i > 0; i--) {
             snake[i] = snake[i - 1];
         }
 
-        
+ 
         switch (direction) {
             case UP:    snake[0].y--; break;
             case DOWN:  snake[0].y++; break;
@@ -52,6 +55,9 @@ int main()
     Dir direction = RIGHT;
     char t;
 
+    int chieuRongKhung = 50; 
+    int chieuCaoKhung = 20;  
+
     while (1){
         if (kbhit()) {
             t = getch();
@@ -59,11 +65,15 @@ int main()
             if (t == 'd' && direction != LEFT)  direction = RIGHT;
             if (t == 'w' && direction != DOWN)  direction = UP;
             if (t == 's' && direction != UP)    direction = DOWN;
-            if (t == 27) break;
+            if (t == 27) break; 
         }
+        
         system("cls");
+        
+        VeKhung(chieuRongKhung, chieuCaoKhung); 
         r.Ve();
         r.DiChuyen(direction);
+        
         Sleep(100);
     }
 
@@ -72,12 +82,21 @@ int main()
 
 
 void gotoxy( int column, int line )
-  {
-  COORD coord;
-  coord.X = column;
-  coord.Y = line;
-  SetConsoleCursorPosition(
-    GetStdHandle( STD_OUTPUT_HANDLE ),
-    coord
-    );
-  }
+{
+    COORD coord;
+    coord.X = column;
+    coord.Y = line;
+    SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ), coord);
+}
+
+
+void VeKhung(int rong, int cao) {
+    for (int i = 0; i <= rong; i++) {
+        gotoxy(i, 0); cout << "#";
+        gotoxy(i, cao); cout << "#";
+    }
+    for (int i = 0; i <= cao; i++) {
+        gotoxy(0, i); cout << "#";
+        gotoxy(rong, i); cout << "#";
+    }
+}
